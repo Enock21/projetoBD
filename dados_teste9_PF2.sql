d5de1e42a411ac3bd40701e3682245cddd63a80d
ufcg04 <joao.melo@ccc.ufcg.edu.br>
2021-05-15 18:35:45 -0300

    arquivo anterior deletado

--- dados_teste9_PF2.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW novaVisao AS
-    SELECT o.codigo, status, p.codigo as codigo_produto, p.nome as nome_produto, cli.nome as nome_cliente, cli.codigo as codigo_cliente
-    FROM ORDEM_DE_COMPRA o , PRODUTO p, CLIENTE cli
-    WHERE o.codigo = p.codigo
-
